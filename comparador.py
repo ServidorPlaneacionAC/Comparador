4a6c1586b61036509372e9e03cde0c139ca603fb
ServidorPlaneacionAC <132586180+ServidorPlaneacionAC@users.noreply.github.com>
2024-01-12 11:03:26 -0500

    Update comparador.py

--- comparador.py
+++ comparador.py
@@ -93,7 +93,11 @@ if archivo_base and archivo_comparar:
 
             ruta_completa = os.path.join(ruta_descargas, "comparacion_datos_maestros.xlsx")
 
-            # Escribir el DataFrame con filas que tienen diferencias
             df_diferencias.to_excel(ruta_completa, sheet_name='Con Diferencias', index=False)
+            df_faltantes.to_excel(ruta_completa, sheet_name='Faltantes en Base', index=False)
+            df_filas_en_comparar_no_en_base.to_excel(ruta_completa, sheet_name='Nuevas en Comparar', index=False)
+            df_base[df_base.index.isin(df_diferencias.index)].to_excel(ruta_completa, sheet_name='Archivo Base', index=False)  # Agregar hoja con el archivo base
 
             st.success(f"Archivo 'comparacion_datos_maestros.xlsx' descargado en la carpeta 'Joy' en D:\\acwagavilan\\Desktop.")
+
+
